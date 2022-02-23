383399021ac3785c6ccbd49fe74e519701747a97
Paleksz <96802620+Hardyzom@users.noreply.github.com>
2022-02-23 11:18:33 +0100

    Add files via upload

---
+++ feb23kilencedik.c
@@ -0,0 +1,16 @@
+#include <stdio.h> //fuggvenyek
+
+int duplaz(int n){
+    return 2*n;
+}
+
+
+int main(){
+
+int a=2;
+printf("Az a valtozo erteke: %d\n",a);
+printf("Az a valtozo duplaja: %d\n",duplaz(a));
+
+return 0;
+
+}
